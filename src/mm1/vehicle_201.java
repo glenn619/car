e813a6f958e0178d229619f329450d95f1236fd8
glenn <casetoolbca201@gmail.com>
2024-04-10 16:37:13 +0530

    commit

---
+++ src/mm1/vehicle_201.java
@@ -0,0 +1,16 @@
+package mm1;
+
+public class vehicle_201 {
+	
+String brand;
+int speed;
+
+public void vehicles_201 (String brand){
+	this.brand=brand;
+	this.speed=0;
+	
+}
+public static void main(String[]args) {
+	
+}
+}
